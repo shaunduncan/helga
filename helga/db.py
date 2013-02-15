4fa05d8e316f728a953c00d121577d390f19e6fb
Shaun Duncan <shaun.duncan@coxinc.com>
2013-02-15 18:03:18 -0500

    Mongo connection should be able to fail gracefully

--- helga/db.py
+++ helga/db.py
@@ -4,8 +4,12 @@ from helga import settings
 
 
 def _connect():
-    client = pymongo.MongoClient(settings.MONGODB['HOST'], settings.MONGODB['PORT'])
-    return client, client[settings.MONGODB['DB']]
+    try:
+        client = pymongo.MongoClient(settings.MONGODB['HOST'], settings.MONGODB['PORT'])
+    except pymongo.errors.ConnectionFailure:
+        return None, None
+    else:
+        return client, client[settings.MONGODB['DB']]
 
 
 client, db = _connect()
