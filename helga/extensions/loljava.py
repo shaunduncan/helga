50007ee27479c872b77c1b6244d9572dc6833703
Shaun Duncan <shaun.duncan@coxinc.com>
2013-02-23 00:03:15 -0500

    Hilariously named java things

--- helga/extensions/loljava.py
+++ helga/extensions/loljava.py
@@ -1,5 +1,144 @@
+import random
+import re
+
 from helga.extensions.base import HelgaExtension
 
 
 class LOLJavaExtension(HelgaExtension):
-    pass
+
+    descriptors = (
+        'Abstract',
+        'Base',
+        'Lazy',
+        'Mutable',
+        'Composite',
+        'Recursive',
+        'Asynchronous',
+        'Shared',
+        'Runnable',
+        'Secure',
+        'Generic',
+        'Internal',
+        'External',
+        'Prioritized',
+    )
+
+    frameworks = (
+        'Swing',
+        'Hibernate',
+        'JDBC',
+        'Spring',
+        'Struts',
+        'Android',
+        'AWT',
+
+        # Give some weight
+        '',
+        '',
+        '',
+        '',
+    )
+
+    things = (
+        'Printer',
+        'Internet',
+        'Synergy',
+        'IRC',
+        'Window',
+        'Network',
+        'TCP',
+        'Protocol',
+        'Data',
+        'Web',
+        'Script',
+        'Command',
+        'Television',
+        'UML',
+        'Null',
+        'Scheduler',
+        'Bandwidth',
+        'Bot',
+        'Computation',
+        'Event'
+        'Color',
+        'Font',
+        'Logger',
+        'Hash',
+        'SOAP',
+        'XML',
+        'JSON',
+        'HTTP',
+        'REST',
+        'Layout',
+        'Cache',
+        'List',
+        'Enum',
+        'Device',
+        'Statement',
+        'Memory',
+        'Timestamp',
+        'Graph',
+        'Result',
+        'Grid',
+        'Category',
+        'Story',
+        'Article',
+        'Model',
+    )
+
+    actions = (
+        'Extraction',
+        'Mutation',
+        'Automation',
+        'Interpolation',
+        'Instantiation',
+        'Translation',
+        'Iteration',
+
+        # Give some weight
+        '',
+        '',
+        '',
+        '',
+    )
+
+    oop = (
+        'Thread',
+        'Bean',
+        'Command',
+        'Prototype',
+        'Factory',
+        'Builder',
+        'Singleton',
+        'Client',
+        'Exception',
+        'Error',
+        'Pool',
+        'Decorator',
+        'Controller',
+        'Proxy',
+        'Module',
+        'Adapter',
+        'Bridge',
+        'Wrapper',
+        'Iterator',
+        'Observer',
+        'Interpreter',
+        'Applet',
+        'Action',
+        'Map',
+        'Queue',
+        'Configuration',
+        'Encoder',
+    )
+
+    def make_bullshit_java_thing(self):
+        return (random.choice(self.descriptors) +
+                random.choice(self.frameworks) +
+                random.choice(self.things + self.oop) +
+                random.choice(self.actions) +
+                random.choice(self.oop))
+
+    def dispatch(self, nick, channel, message, is_public):
+        if re.match(r'(^|.+)java($|[^s]+)', message, re.I):
+            return self.make_bullshit_java_thing()
--- helga/tests/extensions/test_loljava.py
+++ helga/tests/extensions/test_loljava.py
@@ -1,5 +1,23 @@
 from unittest import TestCase
 
+from helga.extensions.loljava import LOLJavaExtension
+from helga.tests.util import mock_bot
+
 
 class LOLJavaExtensionTestCase(TestCase):
-    pass
+
+    def setUp(self):
+        self.loljava = LOLJavaExtension(mock_bot())
+
+    def test_make_bullshit_java_thing(self):
+        assert self.loljava.make_bullshit_java_thing()
+
+    def test_dispatch_returns_message(self):
+        assert self.loljava.dispatch('foo', 'bar', 'java', True)
+        assert self.loljava.dispatch('foo', 'bar', 'lol java', True)
+        assert self.loljava.dispatch('foo', 'bar', 'lol java, crazy', True)
+        assert self.loljava.dispatch('foo', 'bar', '[JAVA]', True)
+
+    def test_dispatch_returns_none(self):
+        assert self.loljava.dispatch('foo', 'bar', 'php', True) is None
+        assert self.loljava.dispatch('foo', 'bar', 'javascript', True) is None
