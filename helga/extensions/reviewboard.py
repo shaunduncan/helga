afe1d61085a0eb5229675182482586d1855567da
Shaun Duncan <justin.caratzas@gmail.com>
2013-02-26 21:31:27 -0500

    Fixed plurals in reviewboard

--- helga/extensions/reviewboard.py
+++ helga/extensions/reviewboard.py
@@ -15,7 +15,7 @@ class ReviewboardExtension(HelgaExtension):
             cr_urls.append(settings.REVIEWBOARD_URL % {'review': match})
 
         if cr_urls:
-            cr_text = 'codereview' + ('s' if len(cr_urls) == 1 else '')
+            cr_text = 'codereview' + ('s' if len(cr_urls) > 1 else '')
             return '%(nick)s might be talking about ' + cr_text + ': ' + ', '.join(cr_urls)
 
     def dispatch(self, nick, channel, message, is_public):
