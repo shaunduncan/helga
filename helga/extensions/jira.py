c24e6f0d7d19420f34e1b635bcdf74f29211b368
Shaun Duncan <shaun.duncan@coxinc.com>
2013-03-01 23:13:27 -0500

    Updating jira extension

--- helga/extensions/jira.py
+++ helga/extensions/jira.py
@@ -3,82 +3,70 @@ import re
 
 from helga import settings
 from helga.db import db
-from helga.extensions.base import ContextualExtension
+from helga.extensions.base import (CommandExtension,
+                                   ContextualExtension)
 from helga.log import setup_logger
 
 
 logger = setup_logger(__name__)
 
 
-class JiraExtension(ContextualExtension):
+class JiraExtension(CommandExtension, ContextualExtension):
+
+    NAME = 'jira'
+
+    usage = '[BOTNICK] jira (add_re|remove_re) <pattern>'
 
     allow_many = True
-
-    def __init__(self, *args, **kwargs):
-        if kwargs.get('load', True):
-            self.jira_pats = set(item['re'] for item in db.jira.find())
-        else:
-            self.jira_pats = set()
-
-        if 'load' in kwargs:
-            del kwargs['load']
-
-        super(JiraExtension, self).__init__(*args, **kwargs)
 
     @property
     def context(self):
         return r'((%s)-[0-9]+)' % '|'.join(self.jira_pats)
 
+    def on(self, event, *args, **kwargs):
+        if event == 'signon':
+            self.jira_pats = set(item['re'] for item in db.jira.find())
+
     def transform_match(self, match):
         return settings.JIRA_URL % {'ticket': match[0]}
 
-    def get_ticket_re(self, command, message, is_public):
-        if is_public:
-            pat = r'^%s jira %s (.+)$'
+    def handle_message(self, opts, message):
+        if opts['add_re']:
+            message.response = self.add_re(opts['<pattern>'])
+        elif opts['remove_re']:
+            message.response = self.remove_re(opts['<pattern>'])
+
+    def add_re(self, pattern):
+        if pattern not in self.jira_pats:
+            logger.info('Adding new JIRA ticket RE: %s' % pattern)
+
+            self.jira_pats.add(pattern)
+            re_doc = {'re': pattern}
+
+            # Store in DB
+            if not db.jira.find(re_doc).count():
+                db.jira.insert(re_doc)
         else:
-            pat = r'^(%s )?jira %s (.+)$'
+            logger.info('JIRA ticket RE already exists: %s' % pattern)
 
-        matches = re.findall(pat % (self.bot.nick, command), message)
+        return '%(nick)s, ' + random.choice(self.add_acks)
 
-        if matches:
-            # Public ['cmd']
-            # Non-public [('nick', 'cmd')]
-            return matches[0] if is_public else matches[0][1]
+    def remove_re(self, pattern):
+        logger.info('Removing JIRA ticket RE: %s' % pattern)
+        self.jira_pats.discard(pattern)
+        db.jira.remove({'re': pattern})
 
-        return None
+        return '%(nick)s, ' + random.choice(self.delete_acks)
 
-    def add_ticket_re(self, message, is_public):
-        new_re = self.get_ticket_re('add_re', message, is_public)
+    def process(self, message):
+        # Try to contextualize
+        self.contextualize(message)
 
-        if new_re:
-            if new_re not in self.jira_pats:
-                logger.info('Adding new JIRA ticket RE: %s' % new_re)
+        if message.has_response:
+            return
 
-                self.jira_pats.add(new_re)
-                re_doc = {'re': new_re}
+        # Try to handle commands
+        opts = self.parse_command(message)
 
-                # Store in DB
-                if not db.jira.find(re_doc).count():
-                    db.jira.insert({'re': new_re})
-            else:
-                logger.info('JIRA ticket RE already exists: %s' % new_re)
-
-            return '%(nick)s, ' + random.choice(self.add_acks)
-
-    def remove_ticket_re(self, message, is_public):
-        rem_re = self.get_ticket_re('remove_re', message, is_public)
-
-        if rem_re:
-            logger.info('Removing JIRA ticket RE: %s' % rem_re)
-            self.jira_pats.discard(rem_re)
-            db.jira.remove({'re': rem_re})
-
-            return '%(nick)s, ' + random.choice(self.delete_acks)
-
-    def dispatch(self, nick, channel, message, is_public):
-        # Return the first-of response
-        return (
-            self.add_ticket_re(message, is_public) or
-            self.remove_ticket_re(message, is_public) or
-            self.contextualize(message)
-        )
+        if self.should_handle_message(opts, message):
+            self.handle_message(opts, message)
