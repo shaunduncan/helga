addaee951b2b1646e09c558b68ebbe615284b71f
Shaun Duncan <justin.caratzas@gmail.com>
2013-03-01 23:28:39 -0500

    Updated reviewboard

--- helga/extensions/reviewboard.py
+++ helga/extensions/reviewboard.py
@@ -4,6 +4,8 @@ from helga.extensions.base import ContextualExtension
 
 class ReviewboardExtension(ContextualExtension):
 
+    NAME = 'reviewboard'
+
     context = r'cr([\d]+)'
     allow_many = True
     response_fmt = '%(nick)s might be talking about codereview: %(response)s'
