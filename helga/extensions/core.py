57d4f9e13e522d64ccad431c5a5563bd08de81b7
Shaun Duncan <shaun.duncan@coxinc.com>
2013-03-04 10:38:38 -0500

    DB backed auto disable extensions

--- helga/extensions/core.py
+++ helga/extensions/core.py
@@ -1,3 +1,4 @@
+from helga.db import db
 from helga.extensions.base import CommandExtension
 from helga.log import setup_logger
 
@@ -28,19 +29,47 @@ class ControlExtension(CommandExtension):
     def list_extensions(self, channel):
         enabled = self.registry.get_enabled(channel)
 
-        logger.info('Currently enabled: %s' % enabled)
-
         # This is converted to multiple lines
         return 'Extensions on this channel: ' + ', '.join(enabled)
 
-    def disable_extension(self, ext_name, channel):
+    def on(self, event, *args, **kwargs):
+        if event == 'signon':
+            for rec in db.disabled_extensions.find():
+                for channel in rec['channels']:
+                    self.disable_extension(rec['extension'], channel, nodb=True)
+
+    def _init_db_rec(self, ext, channel=None):
+        channels = [channel] if channel else []
+        if db.disabled_extensions.find({'extension': ext}).count() == 0:
+            logger.info('Initializing autodisable %s on %s' % (ext, channel))
+            db.disabled_extensions.insert({'extension': ext, 'channels': channels})
+
+    def disable_extension(self, ext_name, channel, nodb=False):
         if self.registry.disable(ext_name, channel):
+            if not nodb:
+                self._init_db_rec(ext_name, channel)
+
+                rec = db.disabled_extensions.find_one({'extension': ext_name})
+                if channel not in rec['channels']:
+                    logger.info('Adding autodisable %s on %s' % (ext_name, channel))
+                    rec['channels'].append(channel)
+                    rec.save()
+
             return self.random_ack()
         else:
             return "Sorry %(nick)s, don't know that one. Try `" + self.bot.nick + " extension list`"
 
-    def enable_extension(self, ext_name, channel):
+    def enable_extension(self, ext_name, channel, nodb=False):
         if self.registry.enable(ext_name, channel):
+            if not nodb:
+                self._init_db_rec(ext_name, channel)
+
+                rec = db.disabled_extensions.find_one({'extension': ext_name})
+                if channel in rec['channels']:
+                    logger.info('Removing autodisable %s on %s' % (ext_name, channel))
+                    rec['channels'].remove(channel)
+                    rec.save()
+
             return self.random_ack()
         else:
             return "Sorry %(nick)s, don't know that one. Try `" + self.bot.nick + " extension list`"
