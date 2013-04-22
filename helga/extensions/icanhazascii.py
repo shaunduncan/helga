d4b72f8c16fd5d1d02b0fa758fce97d2fae64a6f
Vince Veselosky <vince@control-escape.com>
2013-04-22 14:18:26 -0300

    Added the bat signal!

--- helga/extensions/icanhazascii.py
+++ helga/extensions/icanhazascii.py
@@ -89,6 +89,20 @@ class ICanHazAsciiExtension(HelgaExtension):
       /'|  |\\   /|  |`\\
      (,,/:.|.-'-.|.:\\,,)
        (,,,/     \\,,,)""",
+       
+       r'bat.?signal': """
+       _==/          i     i          \==_
+     /XX/            |\___/|            \XX\
+   /XXXX\            |XXXXX|            /XXXX\
+  |XXXXXX\_         _XXXXXXX_         _/XXXXXX|
+ XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX
+|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
+ XXXXXX/^^^^"\XXXXXXXXXXXXXXXXXXXXX/^^^^^\XXXXXX
+  |XXX|       \XXX/^^\XXXXX/^^\XXX/       |XXX|
+    \XX\       \X/    \XXX/    \X/       /XX/
+       "\       "      \X/      "      /" """,
     }
 
     def is_flooded(self, channel):
