4b9ff30d43541e37bef795fe006471cd2ed01f65
Shaun Duncan <shaun.duncan@gmail.com>
2013-06-05 11:58:18 -0400

    Undoing case insenstive usage. It breaks some things

--- helga/tests/extensions/test_base.py
+++ helga/tests/extensions/test_base.py
@@ -69,11 +69,6 @@ class CommandExtensionTestCase(TestCase):
         msg = Mock(message='foo')
         assert self.ext.parse_command(msg)
 
-    def test_parse_command_mixed_case(self):
-        self.ext.usage = 'BOTNICK foo'
-        msg = Mock(message='helga Foo')
-        assert self.ext.parse_command(msg)
-
     def test_should_handle_message_private_without_nick(self):
         msg = Mock(is_public=False)
         opts = {
