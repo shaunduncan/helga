b84b8c1aa6e109b306652f7aebad2434ce521a7d
Shaun Duncan <shaun.duncan@gmail.com>
2013-12-05 11:03:35 -0500

    Test updates and fixes

--- helga/tests/test_plugins.py
+++ helga/tests/test_plugins.py
@@ -12,6 +12,7 @@ class CommandTestCase(TestCase):
 
     def setUp(self):
         self.cmd = plugins.Command('foo', aliases=('bar', 'baz'), help='foo cmd')
+        self.client = Mock(nickname='helga')
 
     def test_init_does_not_overwrite_things(self):
         class MyCommand(plugins.Command):
@@ -25,58 +26,62 @@ class CommandTestCase(TestCase):
         assert cmd.help == 'my command'
 
     def test_parse_handles_main_command(self):
-        assert 'foo' == self.cmd.parse('helga foo')[0]
+        assert 'foo' == self.cmd.parse('helga', 'helga foo')[0]
 
     def test_parse_handles_aliases(self):
-        assert 'bar' == self.cmd.parse('helga bar')[0]
-        assert 'baz' == self.cmd.parse('helga baz')[0]
+        assert 'bar' == self.cmd.parse('helga', 'helga bar')[0]
+        assert 'baz' == self.cmd.parse('helga', 'helga baz')[0]
 
     def test_parse_with_punctuation(self):
-        assert 'foo' == self.cmd.parse('helga: foo')[0]
-        assert 'foo' == self.cmd.parse('helga, foo')[0]
-        assert 'foo' == self.cmd.parse('helga ----> foo')[0]
+        assert 'foo' == self.cmd.parse('helga', 'helga: foo')[0]
+        assert 'foo' == self.cmd.parse('helga', 'helga, foo')[0]
+        assert 'foo' == self.cmd.parse('helga', 'helga ----> foo')[0]
 
     def test_parse_does_not_handle(self):
-        assert '' == self.cmd.parse('helga qux')[0]
+        assert '' == self.cmd.parse('helga', 'helga qux')[0]
 
     def test_parse_returns_args(self):
-        assert ['1', '2', '3'] == self.cmd.parse('helga foo 1 2 3')[1]
+        assert ['1', '2', '3'] == self.cmd.parse('helga', 'helga foo 1 2 3')[1]
 
     def test_process_for_different_command_returns_none(self):
-        assert self.cmd.process('#bots', 'me', 'helga qux') is None
+        assert self.cmd.process(self.client, '#bots', 'me', 'helga qux') is None
 
     def test_process_calls_class_run_method(self):
-        self.cmd.run = lambda chan, nick, msg, cmd, args: 'run'
-        assert 'run' == self.cmd.process('#bots', 'me', 'helga foo')
-
-    def test_process_returns_none_when_typeerror(self):
-        self.cmd.run = Mock(side_effect=TypeError)
-        assert self.cmd.process('#bots', 'me', 'helga foo') is None
+        self.cmd.run = lambda client, chan, nick, msg, cmd, args: 'run'
+        assert 'run' == self.cmd.process(self.client, '#bots', 'me', 'helga foo')
 
     def test_process_calls_custom_runner(self):
         cmd = plugins.Command('foo', aliases=('bar', 'baz'), help='foo command')
-        cmd.run = lambda chan, nick, msg, cmd, args: 'runner'
-        assert 'runner' == cmd.process('#bots', 'me', 'helga foo')
+        cmd.run = lambda client, chan, nick, msg, cmd, args: 'runner'
+        assert 'runner' == cmd.process(self.client, '#bots', 'me', 'helga foo')
+
+    def test_decorator_sets_process_attr(self):
+        @plugins.command('foo')
+        def foo(client, chan, nick, msg, cmd, args):
+            return 'bar'
+
+        assert hasattr(foo, 'process')
 
     def test_decorator_using_command(self):
         @plugins.command('foo')
-        def foo(chan, nick, msg, cmd, args):
+        def foo(client, chan, nick, msg, cmd, args):
             return 'bar'
 
-        assert 'bar' == foo('#bots', 'me', 'helga foo')
+        assert 'bar' == foo.process(self.client, '#bots', 'me', 'helga foo')
 
     def test_decorator_using_alias(self):
         @plugins.command('foo', aliases=['baz'])
-        def foo(chan, nick, msg, cmd, args):
+        def foo(client, chan, nick, msg, cmd, args):
             return 'bar'
 
-        assert 'bar' == foo('#bots', 'me', 'helga baz')
+        assert 'bar' == foo.process(self.client, '#bots', 'me', 'helga baz')
 
 
 class MatchTestCase(TestCase):
 
     def setUp(self):
         self.match = plugins.Match('foo')
+        self.client = Mock()
 
     def test_init_does_not_overwrite_things(self):
         class MyMatch(plugins.Match):
@@ -97,17 +102,24 @@ class MatchTestCase(TestCase):
         self.match.pattern = Mock(side_effect=TypeError)
         assert self.match.match('this is a foo message') is None
 
-    def test_simple_decorator(self):
+    def test_decorator_sets_process_attr(self):
         @plugins.match('foo-(\d+)')
-        def foo(chan, nick, msg, matches):
+        def foo(client, chan, nick, msg, matches):
             return matches[0]
 
-        assert '123' == foo('#bots', 'me', 'this is about foo-123')
+        assert hasattr(foo, 'process')
+
+    def test_simple_decorator(self):
+        @plugins.match('foo-(\d+)')
+        def foo(client, chan, nick, msg, matches):
+            return matches[0]
+
+        assert '123' == foo.process(self.client, '#bots', 'me', 'this is about foo-123')
 
     def test_callable_decorator(self):
         @plugins.match(lambda x: x.startswith('foo'))
-        def foo(chan, nick, msg, matches):
+        def foo(client, chan, nick, msg, matches):
             return 'bar'
 
-        assert 'bar' == foo('#bots', 'me', 'foo at the start')
-        assert foo('#bots', 'me', 'not at the start foo') is None
+        assert 'bar' == foo.process(self.client, '#bots', 'me', 'foo at the start')
+        assert foo.process(self.client, '#bots', 'me', 'not at the start foo') is None
