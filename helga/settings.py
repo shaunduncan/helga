0a2e69e0971b125c310300046b6f6d5c02513b3e
Shaun Duncan <justin.caratzas@gmail.com>
2013-12-06 07:48:27 -0500

    Lots of changes. Removed the old registry, changed settings names. Changed some tests

--- helga/settings.py
+++ helga/settings.py
@@ -9,47 +9,22 @@ SERVER = {
 
 LOG_LEVEL = 'DEBUG'
 
-DEFAULT_NICK = 'helga'
-CHANNELS = (
+NICK = 'helga'
+CHANNELS = [
     ('#bots',),
-)
+]
 
 AUTO_RECONNECT = True
 RATE_LIMIT = None
 
-OPERATORS = ('sduncan',)
+OPERATORS = ['sduncan']
 
-MONGODB = {
+# MongoDB settings
+DATABASE = {
     'HOST': 'localhost',
     'PORT': 27017,
     'DB': 'helga',
 }
-
-
-# Modules and their settings
-EXTENSIONS = (
-    'operator',
-    'reviewboard',
-    'jira',
-    'facts',
-    'haiku',
-    'tanka',
-    'loljava',
-    'oneliner',
-    'no_more_olga',
-    'stfu',
-    'dubstep',
-    'icanhazascii',
-    'mts',
-    'wiki_whois',
-    'giphy',
-)
-
-JIRA_URL = 'https://jira.cmgdigital.com/browse/%(ticket)s'
-WIKI_URL = 'http://intranet.cmgdigital.com/display/~%(user)s/Home'
-REVIEWBOARD_URL = 'http://reviews.ddtc.cmgdigital.com/r/%(review)s'
-
-ALLOW_NICK_CHANGE = False
 
 
 if 'HELGA_SETTINGS' in os.environ:
@@ -57,7 +32,7 @@ if 'HELGA_SETTINGS' in os.environ:
         path = os.environ['HELGA_SETTINGS']
         overrides = __import__(path, {}, {}, [path.split('.')[-1]])
     except ImportError:
-        warnings.warn('Unabled to import HELGA_SETTINGS override. Is it on the path?')
+        warnings.warn('Unabled to import HELGA_SETTINGS override. Is it on sys.path?')
     else:
         this = sys.modules[__name__]
         for attr in filter(lambda x: not x.startswith('_'), dir(overrides)):
---
+++ helga/tests/test_comm.py
@@ -0,0 +1,57 @@
+# -*- coding: utf8 -*-
+import re
+
+from mock import Mock, patch
+from unittest import TestCase
+
+from helga import comm
+
+
+class ClientTestCase(TestCase):
+
+    def setUp(self):
+        self.client = comm.Client()
+
+    def test_parse_nick(self):
+        nick = self.client.parse_nick('foo!~foobar@localhost')
+        assert nick == 'foo'
+
+    @patch('helga.comm.irc.IRCClient')
+    def test_msg_encodes_utf_8(self, irc):
+        uni_str = u'ಠ_ಠ'
+        utf8_str = '\xe0\xb2\xa0_\xe0\xb2\xa0'
+
+        self.client.msg('#foo', uni_str)
+        irc.msg.assertCalledWith('#foo', utf8_str)
+
+    def test_alterCollidedNick(self):
+        self.client.alterCollidedNick('foo')
+        assert re.match(r'foo_[\d]+', self.client.nickname)
+
+    @patch('helga.comm.settings')
+    @patch('helga.comm.smokesignal')
+    def test_signedOn_sends_signal(self, signal, settings):
+        settings.CHANNELS = []
+        self.client.signedOn()
+        signal.emit.assertCalledWith('signon')
+
+    @patch('helga.comm.plugins')
+    def test_privmsg_sends_single_string(self, plugins):
+        self.client.msg = Mock()
+        plugins.registry.process.return_value = ['line1', 'line2']
+
+        self.client.privmsg('foo!~bar@baz', '#bots', 'this is the input')
+
+        args = self.client.msg.call_args[0]
+        assert args[0] == '#bots'
+        assert args[1] == 'line1\nline2'
+
+    @patch('helga.comm.plugins')
+    def test_privmsg_responds_to_user_when_private(self, plugins):
+        self.client.nickname = 'helga'
+        self.client.msg = Mock()
+        plugins.registry.process.return_value = ['line1', 'line2']
+
+        self.client.privmsg('foo!~bar@baz', 'helga', 'this is the input')
+
+        assert self.client.msg.call_args[0][0] == 'foo'
