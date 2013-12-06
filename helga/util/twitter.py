7bf6d720748eb0b998f62251f57c3e9067a39b59
Shaun Duncan <shaun.duncan@gmail.com>
2013-12-06 16:51:45 -0500

    Updated haiku. Need to merge in what tanka does, though

---
+++ helga/util/twitter.py
@@ -0,0 +1,52 @@
+import tweepy
+
+from helga import log, settings
+
+
+logger = log.getLogger(__name__)
+
+
+def is_properly_configured(config):
+    return all([
+        getattr(settings, 'TWITTER_CONSUMER_KEY', None),
+        getattr(settings, 'TWITTER_CONSUMER_SECRET', None),
+        getattr(settings, 'TWITTER_OAUTH_TOKEN', None),
+        getattr(settings, 'TWITTER_OAUTH_TOKEN_SECRET', None),
+    ])
+
+
+def message_max(message, max):
+    if len(message) > max:
+        logger.warning('Message exceeds {0} characters. Truncating'.format(max))
+        message = message[:max]
+    return message
+
+
+def message_140(message):
+    return message_max(message, 140)
+
+
+def get_api():
+    auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(settings.TWITTER_OAUTH_TOKEN, settings.TWITTER_OAUTH_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    return api
+
+
+def tweet(message):
+    if not is_properly_configured():
+        logger.warning('Twitter API requires consumer key, consumer secret, oauth token, oauth secret')
+        return
+
+    message = message_140(message)
+
+    try:
+        logger.info('Tweeting: {0}'.format(message))
+        status = get_api().update_status(message)
+    except:
+        logger.exception('Could not post status')
+    else:
+        tweet_url = 'http://twitter.com/{0}/status/{1}'.format(settings.TWITTER_USERNAME, status.id)
+        logger.info('Tweeted: {0}'.format(tweet_url))
+        return tweet_url
