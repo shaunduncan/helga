6db8928fd10b120d7f5d5e6dc915160ab81e37b9
Shaun Duncan <justin.caratzas@gmail.com>
2013-12-07 13:06:41 -0500

    Update ascii animalz plugin

--- helga/tests/plugins/test_icanhazascii.py
+++ helga/tests/plugins/test_icanhazascii.py
@@ -1,24 +1,26 @@
-import time
+from mock import patch
 
-from unittest import TestCase
-
-from helga.extensions.icanhazascii import ICanHazAsciiExtension
-from helga.tests.util import mock_bot
+from helga.plugins import icanhazascii
 
 
-class ICanHazAsciiExtensionTestCase(TestCase):
+mock_animalz = {r'foo': 'bar'}
 
-    def setUp(self):
-        self.ascii = ICanHazAsciiExtension(mock_bot())
 
-    def test_is_flooded(self):
-        self.ascii.last_used = {'foo': time.time()}
-        assert self.ascii.is_flooded('foo')
+@patch('helga.plugins.icanhazascii.ANIMALZ', mock_animalz)
+def test_find_animal_is_not_too_aggressive():
+    assert icanhazascii.find_animal('this is a foo message') is None
 
-    def test_is_flooded_not_recorded(self):
-        self.ascii.last_used = {}
-        assert not self.ascii.is_flooded('foo')
 
-    def test_is_flooded_not_flooded(self):
-        self.ascii.last_used = {'foo': time.time() - 86400}
-        assert not self.ascii.is_flooded('foo')
+@patch('helga.plugins.icanhazascii.ANIMALZ', mock_animalz)
+def test_find_animal_no_match():
+    assert icanhazascii.find_animal('bar') is None
+
+
+@patch('helga.plugins.icanhazascii.ANIMALZ', mock_animalz)
+def test_find_animal_returns_ascii_string():
+    assert icanhazascii.find_animal('foo') == 'bar'
+
+
+def test_plugin_flood_control():
+    assert icanhazascii.icanhazascii(None, '#bots', 'me', 'foo', 'bar') is not None
+    assert icanhazascii.icanhazascii(None, '#bots', 'me', 'foo', 'bar') is None
