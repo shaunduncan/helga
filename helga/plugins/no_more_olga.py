1e13ec44a69d6e7f191cb8fa4547b8829416af74
Shaun Duncan <justin.caratzas@gmail.com>
2013-12-08 15:20:53 -0500

    Updated no more olga plugin. Some plugin docs

--- helga/plugins/no_more_olga.py
+++ helga/plugins/no_more_olga.py
@@ -1,27 +1,10 @@
 # -*- coding: utf8 -*-
-import random
-import re
-
-from helga import settings
-from helga.extensions.oneliner import OneLinerExtension
-from helga.log import setup_logger
+from helga.plugins import match
 
 
-logger = setup_logger(__name__)
-
-
-def imgur(image):
-    return 'http://i.imgur.com/%s.gif' % image
-
-
-class NoMoreOlgaExtension(OneLinerExtension):
+@match(r'^olga')
+def no_more_olga(client, channel, nick, message, matches):
     """
-    Just tell people not to talk to olga
+    Make sure people are talking to helga and not olga
     """
-
-    NAME = 'no_more_olga'
-
-    responses = {
-        # Direct text responses
-        r'^olga': ("%(nick)s, you should talk to me instead. Olga is no more",),
-    }
+    return '{0}, you should talk to me instead. Olga is no more'.format(nick)
