636b5ecab761cbef375cf4d9917109fe982cfabf
Shaun Duncan <shaun.duncan@gmail.com>
2013-12-10 09:23:42 -0500

    Have the help plugin return a saner message for plugins without a help string

--- helga/plugins/help.py
+++ helga/plugins/help.py
@@ -4,6 +4,10 @@ from helga.plugins import command
 from helga.plugins.core import registry
 
 
+def format_help_string(name, *helps):
+    return '[{0}] {1}'.format(str(name), '. '.join(helps))
+
+
 @command('help', aliases=['halp'],
          help="Show the help string for any commands. Usage: helga help [<plugin>]")
 def help(client, channel, nick, message, cmd, args):
@@ -20,24 +24,29 @@ def help(client, channel, nick, message, cmd, args):
             fn_helps = filter(bool, map(lambda x: getattr(x, 'help', None), plugin._plugins))
             if fn_helps:
                 helps[plugin_name].extend(fn_helps)
+            else:
+                helps[plugin_name].append('No help string for this plugin')
 
     retval = []
     # Send the message to the user
     for key, value in helps.iteritems():
-        retval.append('[{0}] {1}'.format(str(key), '. '.join(value)))
+        retval.append(format_help_string(key, *value))
 
     try:
-        if args[0] not in helps.keys():
-            return "Sorry {0}, I don't know about that plugin".format(nick)
-        else:
-            client.msg(nick, '[{0}] {1}'.format(args[0], '. '.join(helps[args[0]])))
-        return
+        plugin = args[0]
     except IndexError:
         pass
+    else:
+        if plugin not in registry.enabled_plugins[channel]:
+            return "Sorry {0}, I don't know about that plugin".format(nick)
+        elif plugin not in helps.keys():
+            return "Sorry {0}, there's no help string for plugin '{1}'".format(nick, plugin)
+        else:
+            client.msg(nick, format_help_string(plugin, *helps[plugin]))
+        return
 
     if channel != nick:
         client.me(channel, 'whispers to {0}'.format(nick))
 
     retval.insert(0, "{0}, here are the plugins I know about".format(nick))
     client.msg(nick, '\n'.join(retval))
-
