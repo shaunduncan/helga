70e8dcfd00b32267365933cdfed613a4a5826a1f
Shaun Duncan <justin.caratzas@gmail.com>
2013-12-16 07:46:48 -0500

    New core plugin for reminders

---
+++ helga/plugins/reminders.py
@@ -0,0 +1,276 @@
+import datetime
+import re
+
+from itertools import ifilter, imap
+
+import pytz
+import smokesignal
+
+from twisted.internet import reactor
+
+from helga import log, settings
+from helga.db import db
+from helga.plugins import command, random_ack
+
+
+logger = log.getLogger(__name__)
+
+
+in_seconds_map = {
+    'm': 60,
+    'h': 3600,
+    'd': 86400,
+}
+
+# This is how python datetime does it
+days_of_week = {
+    'M':  0,
+    'Tu': 1,
+    'W':  2,
+    'Th': 3,
+    'F':  4,
+    'Sa': 5,
+    'Su': 6,
+}
+days_of_week_lookup = dict((v, k) for k, v in days_of_week.iteritems())
+
+_scheduled = set()
+
+
+@smokesignal.on('signon')
+def init_reminders(client):
+    global _scheduled
+
+    now = datetime.datetime.utcnow()
+    logger.info("Initializing any scheduled reminders")
+
+    for reminder in db.reminders.find():
+        if reminder['_id'] in _scheduled:
+            continue
+
+        _scheduled.add(reminder['_id'])
+        diff = reminder['when'] - now
+        delay = (diff.days * 24 * 3600) + diff.seconds
+        reactor.callLater(delay, _do_reminder, reminder['_id'], client)
+
+
+def readable_time_delta(seconds):
+    days = seconds // 86400
+    seconds -= days * 86400
+    hours = seconds // 3600
+    seconds -= hours * 3600
+    minutes = seconds // 60
+
+    m_suffix = 's' if minutes != 1 else ''
+    h_suffix = 's' if hours != 1 else ''
+    d_suffix = 's' if days != 1 else ''
+
+    retval = '{0} minute{1}'.format(minutes, m_suffix)
+
+    if hours != 0:
+        retval = '{0} hour{1} and {2}'.format(hours, h_suffix, retval)
+
+    if days != 0:
+        retval = '{0} day{1}, {2}'.format(days, d_suffix, retval)
+
+    return retval
+
+
+def _do_reminder(reminder_id, client):
+    global _scheduled
+
+    reminder = db.reminders.find_one(reminder_id)
+    client.msg(reminder['channel'], reminder['message'])
+
+    # If this repeats, figure out the next time
+    if 'repeat' in reminder:
+        now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+        now_dow = now.weekday()
+
+        # Modded range from tomorrow until 1 week from now
+        dow_iter = imap(lambda x: x % 7, xrange(now_dow + 1, now_dow + 8))
+
+        # Filter out any days that aren't in the schedule
+        dow_iter = ifilter(lambda x: x in reminder['repeat'], dow_iter)
+
+        # Get the first one. That's the next day of week
+        try:
+            next_dow = next(dow_iter)
+        except StopIteration:  # How?
+            logger.exception("Somehow, we didn't get a next day of week?")
+            _scheduled.discard(reminder_id)
+            return
+
+        # Get the real day delta
+        day_delta = (next_dow if next_dow > now_dow else next_dow + 7) - now_dow
+
+        # Update the record
+        reminder['when'] += datetime.timedelta(days=day_delta)
+        db.reminders.save(reminder)
+
+        _scheduled.add(reminder_id)
+        reactor.callLater(day_delta * 86400, _do_reminder, reminder_id, client)
+    else:
+        _scheduled.discard(reminder_id)
+        db.reminders.remove(reminder_id)
+
+
+def in_reminder(client, channel, args):
+    """
+    Create a one-time reminder to occur at some amount of minutes, hours, or days
+    in the future. This is used like:
+
+        <sduncan> helga in 12h submit timesheet
+
+    This will create a reminder 12 hours from now with the message "submit timesheet"
+    """
+    global _scheduled
+
+    amount, quantity = int(args[0][:-1]), args[0][-1]
+    message = ' '.join(args[1:])
+
+    if quantity not in in_seconds_map:
+        return "Sorry I didn't understand '{0}'. You must specify m,h,d. Ex: 12m".format(args[0])
+
+    seconds = amount * in_seconds_map[quantity]
+    utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+    delta = datetime.timedelta(seconds=seconds)
+
+    id = db.reminders.insert({
+        'when': utcnow + delta,
+        'message': message,
+        'channel': channel,
+    })
+
+    _scheduled.add(id)
+    reactor.callLater(seconds, _do_reminder, id, client)
+    return 'Reminder set for {0} from now'.format(readable_time_delta(seconds))
+
+
+def at_reminder(client, channel, args):
+    """
+    Schedule a reminder to occur at a specific time. The given time can optionally
+    be specified to occur at a specific timezone, but will default to the value
+    of settings.TIMEZONE if none is specified. Times should be on a 24-hour clock.
+
+    These types of reminders are repeatable, should the last two words of the message
+    be of the form "repeat <days_of_week>" where days_of_week is a single string consisting
+    of any of the following days: M, Tu, W, Th, F, Sa, Su. For example, 'repeat MWF'
+    will repeat a reminder at the same time every Monday, Wednesday, and Friday.
+
+    A full example of how one would use this:
+
+        <sduncan> helga at 13:00 EST standup time repeat MTuWThF
+
+    This will create a reminder "standup time" to occur at 1:00PM Eastern every weekday.
+    """
+    global _scheduled
+
+    now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+
+    # Parse the time it should go off, and the minute offset of the day
+    hh, mm = map(int, args[0].split(':'))
+    next = now.replace(hour=hh, minute=mm)
+
+    # Strip time from args
+    args = args[1:]
+
+    # Default timezone
+    timezone = pytz.timezone(settings.TIMEZONE)
+
+    try:
+        # If there was a timezone passed in
+        timezone = pytz.timezone(args[0])
+    except pytz.UnknownTimeZoneError:
+        pass
+    else:
+        # If so, remove it from args
+        args = args[1:]
+
+    # Now is already UTC current, so just adjust. Next is set without a timezone
+    local_now = now.astimezone(timezone)
+    local_next = next.replace(tzinfo=timezone)
+
+    if local_next <= local_now:
+        local_next += datetime.timedelta(days=1)
+
+    reminder = {
+        'when': local_next.astimezone(pytz.UTC),
+        'channel': channel,
+        'message': ' '.join(args),
+    }
+
+    # Check for 'repeat' arg
+    try:
+        repeat = args[-2] == 'repeat'
+    except IndexError:
+        repeat = False
+
+    if repeat:
+        # If repeating, strip off the last two for the message
+        sched = args[-1]
+        reminder['message'] = ' '.join(args[:-2])
+        repeat_days = sorted([v for k, v in days_of_week.iteritems() if k in sched])
+
+        if not repeat_days:
+            return "I didn't understand '{0}'. You must use any of M,Tu,W,Th,F,Sa,Su. Ex: MWF".format(sched)
+
+        reminder['repeat'] = repeat_days
+
+        for attempt in xrange(7):
+            if reminder['when'].weekday() in repeat_days:
+                break
+            reminder['when'] += datetime.timedelta(days=1)
+
+    id = db.reminders.insert(reminder)
+    diff = reminder['when'] - now
+    delay = (diff.days * 24 * 3600) + diff.seconds
+
+    _scheduled.add(id)
+    reactor.callLater(delay, _do_reminder, id, client)
+    return 'Reminder set for {0} from now'.format(readable_time_delta(delay))
+
+
+def list_reminders(channel):
+    reminders = []
+
+    for reminder in db.reminders.find({'channel': channel}):
+        about = "[{0}] At {1}: '{2}'"
+
+        hash = reminder['_id'][:6]
+        when = reminder['when'].strftime('%m/%d/%y %H:%M %Z').strip()
+
+        about = about.format(hash, when, reminder['message'])
+
+        if 'repeat' in reminder:
+            days = [days_of_week_lookup[value] for value in reminder['repeat']]
+            about = '{0} (Repeat every {1})'.format(about, ','.join(days))
+
+        reminders.append(about)
+
+    return reminders
+
+
+def delete_reminder(channel, hash):
+    rec = db.reminders.find_one({'_id': re.compile(r'^{0}'.format(hash))})
+
+    if rec is not None:
+        db.reminders.remove(rec)
+        return random_ack()
+    else:
+        return "No reminder found with hash '{0}'".format(hash)
+
+
+@command('in', aliases=['at', 'list', 'delete'],
+         help="Schedule reminders. Usage: helga (in ##(m|h|d) <message>|at <HH>:<MM> [<timezone>] "
+              "<message> [repeat <days_of_week]). Ex: 'helga in 12h take out the trash' or "
+              "'helga at 13:00 EST standup time repeat MTuWThF'")
+def reminder(client, channel, nick, message, cmd, args):
+    if cmd == 'in':
+        return in_reminder(args)
+    elif cmd == 'at':
+        return at_reminder(args)
+    elif cmd == 'list':
+        return list_reminders(channel)
+    elif cmd == 'delete':
+        return delete_reminder(channel)
---
+++ helga/tests/plugins/test_reminders.py
@@ -0,0 +1,320 @@
+import datetime
+
+from unittest import TestCase
+
+import pytz
+
+from freezegun import freeze_time
+from mock import Mock, patch
+
+from helga.plugins import reminders
+
+
+class DoReminderTestCase(TestCase):
+
+    def setUp(self):
+        reminders._scheduled.add(1)
+        self.rec = {'channel': '#bots', 'message': 'some message'}
+        self.now = datetime.datetime(day=11, month=12, year=2013)  # A wednesday
+        self.client = Mock()
+
+    @patch('helga.plugins.reminders.db')
+    def test_do_reminder_simple(self, db):
+        db.reminders.find_one.return_value = self.rec
+        reminders._do_reminder(1, self.client)
+
+        assert 1 not in reminders._scheduled
+        db.reminders.remove.assert_called_with(1)
+        self.client.msg.assert_called_with('#bots', 'some message')
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_do_reminder_reschedules_same_week(self, reactor, db):
+        self.rec['when'] = self.now
+        self.rec['repeat'] = [0, 2, 4]  # M, W, F
+        db.reminders.find_one.return_value = self.rec
+
+        with freeze_time(self.now):
+            reminders._do_reminder(1, self.client)
+
+        rec_upd = self.rec.copy()
+        rec_upd['when'] = datetime.datetime(day=13, month=12, year=2013)
+
+        db.reminders.save.assert_called_with(rec_upd)
+        reactor.callLater.assert_called_with(48 * 3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_do_reminder_reschedules_next_week(self, reactor, db):
+        self.rec['when'] = datetime.datetime(day=13, month=12, year=2013)
+        self.rec['repeat'] = [0, 2, 4]  # M, W, F
+        db.reminders.find_one.return_value = self.rec
+
+        with freeze_time(self.rec['when']):
+            reminders._do_reminder(1, self.client)
+
+        rec_upd = self.rec.copy()
+        rec_upd['when'] = datetime.datetime(day=16, month=12, year=2013)
+
+        db.reminders.save.assert_called_with(rec_upd)
+        reactor.callLater.assert_called_with(72 * 3600, reminders._do_reminder, 1, self.client)
+
+
+class InReminderTestCase(TestCase):
+
+    def setUp(self):
+        reminders._scheduled.clear()
+        self.client = Mock()
+        self.now = datetime.datetime(day=13, month=12, year=2013)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_in_reminder_for_minutes(self, reactor, db):
+        db.reminders.insert.return_value = 1
+
+        with freeze_time(self.now):
+            reminders.in_reminder(self.client, '#bots', ['12m', 'this', 'is', 'the', 'message'])
+
+        inserted = db.reminders.insert.call_args[0][0]
+
+        assert inserted['message'] == 'this is the message'
+        assert inserted['channel'] == '#bots'
+        assert reactor.callLater.call_args[0] == (12 * 60, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_in_reminder_for_hours(self, reactor, db):
+        db.reminders.insert.return_value = 1
+
+        with freeze_time(self.now):
+            reminders.in_reminder(self.client, '#bots', ['12h', 'this', 'is', 'the', 'message'])
+
+        inserted = db.reminders.insert.call_args[0][0]
+
+        assert inserted['message'] == 'this is the message'
+        assert inserted['channel'] == '#bots'
+        assert reactor.callLater.call_args[0] == (12 * 3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_in_reminder_for_days(self, reactor, db):
+        db.reminders.insert.return_value = 1
+
+        with freeze_time(self.now):
+            reminders.in_reminder(self.client, '#bots', ['12d', 'this', 'is', 'the', 'message'])
+
+        inserted = db.reminders.insert.call_args[0][0]
+
+        assert inserted['message'] == 'this is the message'
+        assert inserted['channel'] == '#bots'
+        assert reactor.callLater.call_args[0] == (12 * 24 * 3600, reminders._do_reminder, 1, self.client)
+
+
+class AtReminderTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Mock()
+        self.now = datetime.datetime(day=11, month=12, year=2013, hour=12)
+        self.past = datetime.datetime(day=9, month=12, year=2013, hour=12)
+        self.future = datetime.datetime(day=13, month=12, year=2013, hour=12)
+
+        self.tz = pytz.timezone('US/Eastern')
+
+        reminders._scheduled.clear()
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_no_tz_no_repeat_in_future(self, reactor, db):
+        args = ['13:00', 'this is a message']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=5)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=1)
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(self.tz).replace(tzinfo=None)
+
+        assert 'repeat' not in rec
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        reactor.callLater.assert_called_with(1*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_no_tz_no_repeat_in_past(self, reactor, db):
+        args = ['6:00', 'this is a message']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=5)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=18)
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(self.tz).replace(tzinfo=None)
+
+        assert 'repeat' not in rec
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        reactor.callLater.assert_called_with(18*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_tz_no_repeat_in_future(self, reactor, db):
+        args = ['13:00', 'US/Central', 'this is a message']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=6)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=1)
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(pytz.timezone('US/Central')).replace(tzinfo=None)
+
+        assert 'repeat' not in rec
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        reactor.callLater.assert_called_with(1*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_tz_no_repeat_in_past(self, reactor, db):
+        args = ['6:00', 'US/Central', 'this is a message']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=6)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=18)
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(pytz.timezone('US/Central')).replace(tzinfo=None)
+
+        assert 'repeat' not in rec
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        reactor.callLater.assert_called_with(18*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_tz_repeat_in_future(self, reactor, db):
+        args = ['13:00', 'US/Central', 'this is a message', 'repeat', 'MWF']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=6)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=1)
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(pytz.timezone('US/Central')).replace(tzinfo=None)
+
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        assert rec['repeat'] == [0, 2, 4]
+        reactor.callLater.assert_called_with(1*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_tz_repeat_in_past(self, reactor, db):
+        args = ['6:00', 'US/Central', 'this is a message', 'repeat', 'MWF']
+        db.reminders.insert.return_value = 1
+
+        # Account for UTC difference
+        with freeze_time(self.now + datetime.timedelta(hours=6)):
+            reminders.at_reminder(self.client, '#bots', args)
+
+        rec = db.reminders.insert.call_args[0][0]
+        expect = self.now + datetime.timedelta(hours=42)  # We expect it to be on Friday now
+
+        # 'when' will be stored as UTC timestamp
+        when = rec['when'].astimezone(pytz.timezone('US/Central')).replace(tzinfo=None)
+
+        assert when == expect
+        assert rec['channel'] == '#bots'
+        assert rec['message'] == 'this is a message'
+        assert rec['repeat'] == [0, 2, 4]
+        reactor.callLater.assert_called_with(42*3600, reminders._do_reminder, 1, self.client)
+
+    @patch('helga.plugins.reminders.db')
+    @patch('helga.plugins.reminders.reactor')
+    def test_invalid_days_returns_warning(self, reactor, db):
+        # Invalid chars
+        args = ['6:00', 'US/Central', 'this is a message', 'repeat', 'XYZ']
+        assert "I didn't understand" in reminders.at_reminder(self.client, '#bots', args)
+        assert not db.reminders.insert.called
+        assert not reactor.callLater.called
+
+        # No chars
+        args = ['6:00', 'US/Central', 'this is a message', 'repeat', '']
+        assert "I didn't understand" in reminders.at_reminder(self.client, '#bots', args)
+        assert not db.reminders.insert.called
+        assert not reactor.callLater.called
+
+
+class ReadableTimeTestCase(TestCase):
+
+    def test_readable_time_delta_minutes_only(self):
+        ret = reminders.readable_time_delta(610)
+        assert ret == '10 minutes'
+
+    def test_readable_time_delta_hours_and_minutes(self):
+        ret = reminders.readable_time_delta((3 * 3600) + 610)
+        assert ret == '3 hours and 10 minutes'
+
+    def test_readable_time_delta_days_hours_and_minutes(self):
+        ret = reminders.readable_time_delta((8 * 86400) + (3 * 3600) + 610)
+        assert ret == '8 days, 3 hours and 10 minutes'
+
+    def test_readable_time_delta_singular_minutes(self):
+        ret = reminders.readable_time_delta(65)
+        assert ret == '1 minute'
+
+    def test_readable_time_delta_singular_hours_and_minutes(self):
+        ret = reminders.readable_time_delta((1 * 3600) + 65)
+        assert ret == '1 hour and 1 minute'
+
+    def test_readable_time_delta_singular_days_hours_and_minutes(self):
+        ret = reminders.readable_time_delta((1 * 86400) + (1 * 3600) + 65)
+        assert ret == '1 day, 1 hour and 1 minute'
+
+
+class ListRemindersTestCase(TestCase):
+
+    def setUp(self):
+        self.rec = {
+            '_id': '1234567890abcdefg',
+            'when': datetime.datetime(year=2013, month=12, day=11, hour=13, minute=15, tzinfo=pytz.UTC),
+            'message': 'Standup Time!',
+        }
+
+    @patch('helga.plugins.reminders.db')
+    def test_simple(self, db):
+        db.reminders.find.return_value = [self.rec]
+        ret = reminders.list_reminders('#bots')[0]
+        assert ret == "[123456] At 12/11/13 13:15 UTC: 'Standup Time!'"
+
+    @patch('helga.plugins.reminders.db')
+    def test_with_repeats(self, db):
+        self.rec['repeat'] = [0, 2, 4]
+        db.reminders.find.return_value = [self.rec]
+        ret = reminders.list_reminders('#bots')[0]
+        assert ret == "[123456] At 12/11/13 13:15 UTC: 'Standup Time!' (Repeat every M,W,F)"
