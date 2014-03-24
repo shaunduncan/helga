15adb23a7c5d5ca1ad4986b132fbec1b5294bd74
Adam Coddington <me@adamcoddington.net>
2014-03-23 19:33:05 -0700

    Minor alterations to setup.py/etc to allow PyPI distribution.

--- setup.py
+++ setup.py
@@ -1,24 +1,52 @@
+import os.path
 from setuptools import setup, find_packages
 
 version = '1.2'
 
+
+requirements = []
+with open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'requirements.txt',
+    ),
+    'r'
+) as in_:
+    for line in in_.readlines():
+        if not line.strip() or line.startswith('#') or line.startswith('-e'):
+            # Unfortunately, -e requirements can't be listed
+            continue
+        requirements.append(line.strip())
+
+
 setup(name="helga",
       version=version,
       description=('IRC bot using twisted'),
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: IRC',
-                   'Intended Audience :: Twisted Developers, IRC Bot Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: IRC Bots'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Topic :: Communications :: Chat :: Internet Relay Chat'
+          'Framework :: Twisted',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='irc bot',
       author='Shaun Duncan',
       author_email='shaun.duncan@gmail.com',
       url='https://github.com/shaunduncan/helga',
       license='MIT',
       packages=find_packages(),
+      install_requires=requirements,
+      tests_require=[
+          'freezegun',
+          'mock',
+          'pretend',
+          'tox',
+          'nose'
+      ],
+      test_suite='nose.collector',  # I'm not sure how to wire-up tox here :-/
       entry_points = dict(
           helga_plugins=[
               'announcements = helga.plugins.announcements:AnnouncementPlugin',
