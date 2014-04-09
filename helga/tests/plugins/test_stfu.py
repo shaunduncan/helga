44661413f80650b52e15b57a2a49b3224adb6455
Shaun Duncan <shaun.duncan@gmail.com>
2014-04-09 09:43:50 -0400

    Fix tests for stfu plugin

--- helga/tests/plugins/test_stfu.py
+++ helga/tests/plugins/test_stfu.py
@@ -1,45 +1,49 @@
-from mock import patch
+from mock import Mock, patch
 from pretend import stub
 
 from helga.plugins import stfu
 
 
-def test_stfu_preprocess_does_nothing():
+def test_stfu_postprocess_does_nothing():
     stfu.silenced = set()
     client = stub(nickname='helga')
-    chan, nick, msg = stfu.stfu(client, '#bots', 'me', 'foo')
-    assert msg == 'foo'
+    assert 'response' == stfu.stfu(client, '#bots', 'me', 'foo', 'response')
 
 
-def test_stfu_preprocess_blanks_message_when_silenced():
+def test_stfu_postprocess_blanks_response_when_silenced():
     stfu.silenced = set(['#bots'])
     client = stub(nickname='helga')
-    chan, nick, msg = stfu.stfu(client, '#bots', 'me', 'foo')
-    assert msg == ''
+    assert stfu.stfu(client, '#bots', 'me', 'foo', 'response') is None
 
 
 def test_stfu_snark_on_private_message():
+    client = Mock()
     stfu.silenced = set()
-    resp = stfu.stfu(stub(nickname='helga'), 'me', 'me', 'helga stfu', 'stfu', [])
+    stfu.stfu(client, 'me', 'me', 'helga stfu', 'stfu', [])
+
+    chan, resp = client.msg.call_args[0]
     assert resp in map(lambda x: x.format(nick='me'), stfu.snarks)
 
 
 def test_stfu_silences_channel():
+    client = Mock()
     stfu.silenced = set()
-    resp = stfu.stfu(stub(nickname='helga'), '#bots', 'me', 'helga stfu', 'stfu', [])
+    stfu.stfu(client, '#bots', 'me', 'helga stfu', 'stfu', [])
+
+    chan, resp = client.msg.call_args[0]
     assert resp in map(lambda x: x.format(nick='me'), stfu.silence_acks)
     assert '#bots' in stfu.silenced
 
 
 @patch('helga.plugins.stfu.reactor')
 def test_stfu_for_some_time(reactor):
-    client = stub(nickname='helga')
+    client = Mock()
     stfu.stfu(client, '#bots', 'me', 'helga stfu for 30', 'stfu', ['for', '30'])
     reactor.callLater.assertCalledWith(30*60, stfu.auto_unsilence, client, '#bots', 30*60)
 
 
 def test_stfu_unsilences_channel():
+    client = Mock()
     stfu.silenced = set(['#bots'])
-    resp = stfu.stfu(stub(nickname='helga'), '#bots', 'me', 'helga speak', 'speak', [])
-    assert resp in map(lambda x: x.format(nick='me'), stfu.unsilence_acks)
+    stfu.stfu(client, '#bots', 'me', 'helga speak', 'speak', [])
     assert '#bots' not in stfu.silenced
