6e4eaee798b868c4af51058dac929471dde27ae9
Alfredo Deza <justin.caratzas@gmail.com>
2014-07-25 09:03:59 -0400

    add a test class for MeantToSay

--- helga/tests/plugins/test_meant_to_say.py
+++ helga/tests/plugins/test_meant_to_say.py
@@ -30,3 +30,82 @@ def test_meant_to_say_none_when_not_modified():
     client.last_message['#bots']['me'] = 'this is a message'
 
     assert meant_to_say.meant_to_say(client, '#bots', 'me', 's/foo/bar', [('foo', 'bar')]) is None
+
+
+class TestMeantToSay(object):
+
+    def setup(self):
+        self.client = stub(last_message=defaultdict(dict))
+
+    def test_single_replacement(self):
+        self.client.last_message['#bots']['me'] = 'this is a foo message foo'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/',
+            [('foo', 'bar', '/')]
+        )
+        assert resp == 'me meant to say: this is a bar message foo'
+
+    def test_g(self):
+        self.client.last_message['#bots']['me'] = 'this is a foo message foo'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/g',
+            [('foo', 'bar', '/g')]
+        )
+        assert resp == 'me meant to say: this is a bar message bar'
+
+    def test_i(self):
+        self.client.last_message['#bots']['me'] = 'this is a FOO message'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/i',
+            [('foo', 'bar', '/i')]
+        )
+        assert resp == 'me meant to say: this is a bar message'
+
+    def test_i_with_garbage_flags(self):
+        self.client.last_message['#bots']['me'] = 'this is a FOO message'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/iasdf',
+            [('foo', 'bar', '/iasdf')]
+        )
+        assert resp == 'me meant to say: this is a bar message'
+
+    def test_gi(self):
+        self.client.last_message['#bots']['me'] = 'this is a FOO message foo'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/gi',
+            [('foo', 'bar', '/gi')]
+        )
+        assert resp == 'me meant to say: this is a bar message bar'
+
+    def test_gi_with_garbage_flags(self):
+        self.client.last_message['#bots']['me'] = 'this is a FOO message foo'
+
+        resp = meant_to_say.meant_to_say(
+            self.client,
+            '#bots',
+            'me',
+            's/foo/bar/gic',
+            [('foo', 'bar', '/gic')]
+        )
+        assert resp == 'me meant to say: this is a bar message bar'
+
