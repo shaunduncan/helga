30c1f67e1d1c754068a2a1c6a6154ecc4a0bfd3b
Shaun Duncan <justin.caratzas@gmail.com>
2014-08-27 09:53:25 -0400

    Adding unicode tests and coverate for comm

--- helga/tests/test_comm.py
+++ helga/tests/test_comm.py
@@ -1,10 +1,38 @@
 # -*- coding: utf8 -*-
 import re
 
-from mock import Mock, patch
+from mock import Mock, call, patch
 from unittest import TestCase
 
 from helga import comm
+
+
+class FactoryTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = comm.Factory()
+
+    def test_build_protocol(self):
+        client = self.factory.buildProtocol('address')
+        assert client.factory == self.factory
+
+    @patch('helga.comm.settings')
+    def test_client_connection_lost_retries(self, settings):
+        settings.AUTO_RECONNECT = True
+        connector = Mock()
+        self.factory.clientConnectionLost(connector, Exception)
+        assert connector.connect.called
+
+    @patch('helga.comm.settings')
+    def test_client_connection_lost_raises(self, settings):
+        settings.AUTO_RECONNECT = False
+        connector = Mock()
+        self.assertRaises(Exception, self.factory.clientConnectionLost, connector, Exception)
+
+    @patch('helga.comm.reactor')
+    def test_client_connection_failed(self, reactor):
+        self.factory.clientConnectionFailed(Mock(), reactor)
+        assert reactor.stop.called
 
 
 class ClientTestCase(TestCase):
@@ -15,6 +43,10 @@ class ClientTestCase(TestCase):
     def test_parse_nick(self):
         nick = self.client.parse_nick('foo!~foobar@localhost')
         assert nick == 'foo'
+
+    def test_parse_nick_unicode(self):
+        nick = self.client.parse_nick(u'☃!~foobar@localhost')
+        assert nick == u'☃'
 
     @patch('helga.comm.irc.IRCClient')
     def test_me_converts_from_unicode(self, irc):
@@ -34,6 +66,29 @@ class ClientTestCase(TestCase):
     def test_alterCollidedNick(self):
         self.client.alterCollidedNick('foo')
         assert re.match(r'foo_[\d]+', self.client.nickname)
+
+        # Should take the first part up to '_'
+        self.client.alterCollidedNick('foo_bar')
+        assert re.match(r'foo_[\d]+', self.client.nickname)
+
+    @patch('helga.comm.settings')
+    def test_signedOn(self, settings):
+        snowman = u'☃'
+        bytes = '\xe2\x98\x83'
+
+        settings.CHANNELS = [
+            ('#bots',),
+            ('#foo', 'bar'),
+            (u'#baz', snowman),  # Handles unicode gracefully?
+        ]
+
+        with patch.object(self.client, 'join') as join:
+            self.client.signedOn()
+            assert join.call_args_list == [
+                call('#bots'),
+                call('#foo', 'bar'),
+                call('#baz', bytes),
+            ]
 
     @patch('helga.comm.settings')
     @patch('helga.comm.smokesignal')
@@ -62,3 +117,81 @@ class ClientTestCase(TestCase):
         self.client.privmsg('foo!~bar@baz', 'helga', 'this is the input')
 
         assert self.client.msg.call_args[0][0] == 'foo'
+
+    @patch('helga.comm.settings')
+    @patch('helga.comm.irc.IRCClient')
+    def test_connectionMade(self, irc, settings):
+        self.client.connectionMade()
+        irc.connectionMade.assert_called_with(self.client)
+
+    @patch('helga.comm.settings')
+    @patch('helga.comm.irc.IRCClient')
+    def test_connectionLost(self, irc, settings):
+        self.client.connectionLost('an error...')
+        irc.connectionLost.assert_called_with(self.client, 'an error...')
+
+    @patch('helga.comm.settings')
+    @patch('helga.comm.irc.IRCClient')
+    def test_connectionLost_handles_unicode(self, irc, settings):
+        snowman = u'☃'
+        bytes = '\xe2\x98\x83'
+        self.client.connectionLost(snowman)
+        irc.connectionLost.assert_called_with(self.client, bytes)
+
+    @patch('helga.comm.smokesignal')
+    def test_joined(self, signal):
+        # Test str and unicode
+        for channel in ('foo', u'☃'):
+            assert channel not in self.client.channels
+            self.client.joined(channel)
+            assert channel in self.client.channels
+            signal.emit.assert_called_with('join', self.client, channel)
+
+    @patch('helga.comm.smokesignal')
+    def test_left(self, signal):
+        # Test str and unicode
+        for channel in ('foo', u'☃'):
+            self.client.channels.add(channel)
+            self.client.left(channel)
+            assert channel not in self.client.channels
+            signal.emit.assert_called_with('left', self.client, channel)
+
+    def test_kickedFrom(self):
+        # Test str and unicode
+        for channel in ('foo', u'☃'):
+            self.client.channels.add(channel)
+            self.client.kickedFrom(channel, 'me', 'no bots allowed')
+            assert channel not in self.client.channels
+
+    def test_on_invite(self):
+        with patch.object(self.client, 'join') as join:
+            self.client.nickname = 'helga'
+            self.client.on_invite('me', 'helga', '#bots')
+            assert join.called
+
+    def test_on_invite_ignores_other_invites(self):
+        with patch.object(self.client, 'join') as join:
+            self.client.nickname = 'helga'
+            self.client.on_invite('me', 'someone_else', '#bots')
+            assert not join.called
+
+    def test_irc_unknown(self):
+        with patch.object(self.client, 'on_invite') as on_invite:
+            self.client.irc_unknown('me', 'INVITE', ['helga', '#bots'])
+            on_invite.assert_called_with('me', 'helga', '#bots')
+
+            on_invite.reset_mock()
+            self.client.irc_unknown('me', 'SOME_COMMAND', [])
+            assert not on_invite.called
+
+    @patch('helga.comm.smokesignal')
+    def test_userJoined(self, signal):
+        user = 'helga!helgabot@127.0.0.1'
+        self.client.userJoined(user, '#bots')
+        signal.emit.assert_called_with('user_joined', self.client, 'helga', '#bots')
+
+    @patch('helga.comm.smokesignal')
+    def test_userLeft(self, signal):
+        user = 'helga!helgabot@127.0.0.1'
+        self.client.userLeft(user, '#bots')
+        signal.emit.assert_called_with('user_left', self.client, 'helga', '#bots')
