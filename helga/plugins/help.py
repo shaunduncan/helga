f48513c260b6184c41827a644d569dd7c75234d5
Shaun Duncan <shaun.duncan@gmail.com>
2014-08-26 22:12:02 -0400

    Added tests and coverage for unicode support of help plugin

--- helga/plugins/help.py
+++ helga/plugins/help.py
@@ -5,35 +5,29 @@ from helga.plugins.core import registry
 
 
 def format_help_string(name, *helps):
-    return '[{0}] {1}'.format(str(name), '. '.join(helps))
+    return u'[{0}] {1}'.format(name, '. '.join(helps))
 
 
 @command('help', aliases=['halp'],
          help="Show the help string for any commands. Usage: helga help [<plugin>]")
 def help(client, channel, nick, message, cmd, args):
     helps = defaultdict(list)
+    default_help = u'No help string for this plugin'
+
     for plugin_name in registry.enabled_plugins[channel]:
         try:
             plugin = registry.plugins[plugin_name]
         except KeyError:
-            helps[plugin_name].append("Unable to load plugin '{0}'".format(plugin_name))
+            continue
 
         # A simple object
-        if hasattr(plugin, 'help') and plugin.help:
-            helps[plugin_name].append(plugin.help)
+        if hasattr(plugin, 'help'):
+            helps[plugin_name].append(plugin.help or default_help)
 
         # A decorated function
         elif hasattr(plugin, '_plugins'):
             fn_helps = filter(bool, map(lambda x: getattr(x, 'help', None), plugin._plugins))
-            if fn_helps:
-                helps[plugin_name].extend(fn_helps)
-            else:
-                helps[plugin_name].append('No help string for this plugin')
-
-    retval = []
-    # Send the message to the user
-    for key, value in helps.iteritems():
-        retval.append(format_help_string(key, *value))
+            helps[plugin_name].extend(fn_helps or [default_help])
 
     try:
         plugin = args[0]
@@ -41,9 +35,9 @@ def help(client, channel, nick, message, cmd, args):
         pass
     else:
         if plugin not in registry.enabled_plugins[channel]:
-            return "Sorry {0}, I don't know about that plugin".format(nick)
+            return u"Sorry {0}, I don't know about that plugin".format(nick)
         elif plugin not in helps.keys():
-            return "Sorry {0}, there's no help string for plugin '{1}'".format(nick, plugin)
+            return u"Sorry {0}, there's no help string for plugin '{1}'".format(nick, plugin)
 
         # Single plugin, it's probably ok in the public channel
         return format_help_string(plugin, *helps[plugin])
@@ -51,5 +45,10 @@ def help(client, channel, nick, message, cmd, args):
     if channel != nick:
         client.me(channel, 'whispers to {0}'.format(nick))
 
-    retval.insert(0, "{0}, here are the plugins I know about".format(nick))
-    client.msg(nick, '\n'.join(retval))
+    retval = []
+    # Send the message to the user
+    for key, value in helps.iteritems():
+        retval.append(format_help_string(key, *value))
+
+    retval.insert(0, u"{0}, here are the plugins I know about".format(nick))
+    client.msg(nick, u'\n'.join(retval))
