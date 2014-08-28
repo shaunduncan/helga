729926ab49d68c2b317022a6810b1d0c61e98525
Shaun Duncan <shaun.duncan@gmail.com>
2014-08-28 17:57:09 -0400

    Adding tests for the db module

---
+++ helga/tests/test_db.py
@@ -0,0 +1,56 @@
+from mock import patch, Mock
+
+from helga import db
+from pymongo.errors import ConnectionFailure
+
+
+@patch('helga.db.MongoClient')
+@patch('helga.db.settings')
+def test_connect_returns_none_on_failure(settings, mongo):
+    settings.DATABASE = {
+        'HOST': 'localhost',
+        'PORT': '1234',
+    }
+
+    mongo.side_effect = ConnectionFailure
+    assert db._connect() == (None, None)
+
+
+@patch('helga.db.MongoClient')
+@patch('helga.db.settings')
+def test_connect_authenticates(settings, mongo):
+    settings.DATABASE = {
+        'HOST': 'localhost',
+        'PORT': '1234',
+        'USERNAME': 'foo',
+        'PASSWORD': 'bar',
+        'DB': 'baz',
+    }
+
+    mongo.return_value = mongo
+
+    database = Mock()
+    mongo.__getitem__ = Mock()
+    mongo.__getitem__.return_value = database
+
+    db._connect()
+    database.authenticate.assert_called_with('foo', 'bar')
+
+
+@patch('helga.db.MongoClient')
+@patch('helga.db.settings')
+def test_connect(settings, mongo):
+    settings.DATABASE = {
+        'HOST': 'localhost',
+        'PORT': '1234',
+        'DB': 'baz',
+    }
+
+    mongo.return_value = mongo
+
+    database = Mock()
+    mongo.__getitem__ = Mock()
+    mongo.__getitem__.return_value = database
+
+    assert db._connect() == (mongo, database)
+    mongo.__getitem__.assert_called_with('baz')
