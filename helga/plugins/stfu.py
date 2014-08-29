795256c38d6009572c3049e89707d93c747c4055
Shaun Duncan <justin.caratzas@gmail.com>
2014-08-28 23:17:43 -0400

    Added tests and unicode support for stfu plugin

--- helga/plugins/stfu.py
+++ helga/plugins/stfu.py
@@ -7,24 +7,24 @@ from helga.plugins import command, preprocessor
 
 
 silence_acks = (
-    'silence is golden',
-    'shutting up',
-    'biting my tongue',
-    'fine, whatever',
+    u'silence is golden',
+    u'shutting up',
+    u'biting my tongue',
+    u'fine, whatever',
 )
 
 unsilence_acks = (
-    'speaking once again',
-    'did you miss me?',
-    'FINALLY',
-    'thanks {nick}, i was getting bored'
+    u'speaking once again',
+    u'did you miss me?',
+    u'FINALLY',
+    u'thanks {nick}, i was getting bored'
 )
 
 snarks = (
-    'why would you want to do that {nick}?',
-    'do you really despise me that much {nick}?',
-    'whatever i do what i want',
-    'no can do, i love the sound of my own voice',
+    u'why would you want to do that {nick}?',
+    u'do you really despise me that much {nick}?',
+    u'whatever i do what i want',
+    u'no can do, i love the sound of my own voice',
 )
 
 # Set of silenced channels
@@ -36,7 +36,7 @@ def auto_unsilence(client, channel, length):
 
     if channel in silenced:
         silenced.discard(channel)
-        client.msg(channel, "Speaking again after waiting {0} minutes".format(length//60))
+        client.msg(channel, u"Speaking again after waiting {0} minutes".format(length//60))
 
 
 @preprocessor
@@ -48,15 +48,15 @@ def stfu(client, channel, nick, message, *args):
     # Handle the message preprocesor
     if len(args) == 0:
         # Duh, don't silence the speak command
-        is_speak = bool(re.findall(r'^{0}\W*\s(speak)$'.format(client.nickname), message))
+        is_speak = bool(re.findall(ur'^{0}\W*\s(speak)$'.format(client.nickname), message))
 
         if channel in silenced and not is_speak:
-            message = ''
+            message = u''
 
         return channel, nick, message
 
     elif len(args) == 2:
-        resp = ''
+        resp = u''
 
         if channel == nick:  # Private message
             resp = random.choice(snarks)
@@ -72,7 +72,7 @@ def stfu(client, channel, nick, message, *args):
                     pass
                 else:
                     reactor.callLater(length, auto_unsilence, client, channel, length)
-                    resp = "OK {0}, I'll be back in {1} min".format(nick, cmdargs[1])
+                    resp = u"OK {0}, I'll be back in {1} min".format(nick, cmdargs[1])
         elif args[0] == 'speak':
             if channel not in silenced:
                 return None
