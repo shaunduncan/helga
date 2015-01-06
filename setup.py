3e461c715c4e855fcc9dcbd42520080be64b4cb2
Shaun Duncan <justin.caratzas@gmail.com>
2015-01-06 09:46:52 -0500

    Added changelog notes and setup.py fix. Bump to 1.6.3

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
+import functools
+import pkg_resources
 import subprocess
 import sys
 
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-from pip.download import PipSession
-from pip.req import parse_requirements
+from pip.req import parse_requirements as parse_reqs
 
 import helga
 
 
+# Compatibility with older versions of pip
+pip_dist = pkg_resources.get_distribution('pip')
+pip_version = tuple(map(int, pip_dist.version.split('.')))
+
+# Use a base partial that will be updated depending on the version of pip
+parse_requirements = functools.partial(parse_reqs, options=None)
+
+if pip_version < (1, 2):
+    # pip versions before 1.2 require an options keyword for using it outside
+    # of invoking a pip shell command
+    from pip.baseparser import parser
+    parse_requirements.keywords['options'] = parser.parse_args()[0]
+
+if pip_version >= (1, 5):
+    # pip 1.5 introduced a session kwarg that is required in later versions
+    from pip.download import PipSession
+    parse_requirements.keywords['session'] = PipSession()
+
+
+# If installing on python 2.6, we need to install the argparse backport
 extra_requires = []
 if sys.version_info[:2] == (2, 6):
     extra_requires = ['argparse==1.3.0']
@@ -51,7 +72,7 @@ setup(name=helga.__title__,
           'helga': ['webhooks/logger/*.mustache'],
       },
       install_requires=[
-          str(req.req) for req in parse_requirements('requirements.txt', session=PipSession())
+          str(req.req) for req in parse_requirements('requirements.txt')
       ] + extra_requires,
       tests_require=[
           'freezegun',
@@ -60,8 +81,8 @@ setup(name=helga.__title__,
           'tox',
           'pytest',
       ],
-      cmdclass = {'test': PyTest},
-      entry_points = dict(
+      cmdclass={'test': PyTest},
+      entry_points=dict(
           helga_plugins=[
               'dubstep      = helga.plugins.dubstep:dubstep',
               'facts        = helga.plugins.facts:facts',
