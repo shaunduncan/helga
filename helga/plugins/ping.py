51f1418cf9c2e802bcb93d45188e683eb519dea2
Shaun Duncan <shaun.duncan@gmail.com>
2015-03-02 22:53:37 -0500

    Docstring for the ping plugin

--- helga/plugins/ping.py
+++ helga/plugins/ping.py
@@ -3,4 +3,7 @@ from helga.plugins import command
 
 @command('ping', help='A very simple PING plugin. Response with pong. Usage: helga ping')
 def ping(client, channel, nick, message, cmd, args):
+    """
+    Respond to a ping with a pong
+    """
     return u'pong'
