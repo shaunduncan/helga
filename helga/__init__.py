09f9afad3a019763734355b2d1cce0efa3cc9380
Shaun Duncan <shaun.duncan@gmail.com>
2015-03-05 18:33:06 -0500

    Updated PyPI description and keywords for 1.7

--- helga/__init__.py
+++ helga/__init__.py
@@ -5,4 +5,4 @@ __version__ = '1.7.0'
 __author__ = 'Shaun Duncan'
 __license__ = 'MIT/GPLv3'
 __copyright__ = 'Copyright {0} Shaun Duncan'.format(dt.now().strftime('%Y'))
-__description__ = 'A full-featured IRC bot for Python/Twisted with plugin support'
+__description__ = 'A full-featured chat bot for Python 2.6/2.7 with plugin support'
