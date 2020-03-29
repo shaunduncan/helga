374daf4b5c49f3f316629cedc8a990fe7b8b0a4d
Justin Caratzas <jcaratza@redhat.com>
2020-03-29 12:06:06 -0400

    bump version to 2.0.0 + changelog for py3 upgrade

--- helga/__init__.py
+++ helga/__init__.py
@@ -1,8 +1,8 @@
 from datetime import datetime as dt
 
 __title__ = 'helga'
-__version__ = '1.7.13'
+__version__ = '2.0.0'
 __author__ = 'Shaun Duncan'
 __license__ = 'MIT/GPLv3'
 __copyright__ = 'Copyright {0} Shaun Duncan'.format(dt.now().strftime('%Y'))
-__description__ = 'A full-featured chat bot for Python 2.7 with plugin support'
+__description__ = 'A full-featured chat bot for Python 3 with plugin support'
